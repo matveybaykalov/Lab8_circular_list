4d735deddf4961df5d4caa10eebdf3e160ba05e5
matveybaykalov <70635348+matveybaykalov@users.noreply.github.com>
2020-12-15 12:45:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ struct Node{
 };
 
 template<typename tn> //tn тип данных переменной value
-struct List{
+class List{
 private:
     Node<tn>* first = nullptr;
     Node<tn>* last = nullptr;
@@ -22,39 +22,47 @@ private:
         }
         return TempNode;
     }
-public:
-    void push_back(tn value){
+    Node<tn>* PushElement (tn value){
+        auto *node = new Node<tn>;
         if (first == nullptr){
-            auto *node = new Node<tn>;
-            node->prev = nullptr;
-            node->next = nullptr;
-            node->value = value;
             first = node;
             last = node;
         } else {
-            auto *node = new Node<tn>;
-            node->prev = last;
-            node->next = first;
-            node->value = value;
             last->next = node;
-            last = node;
-        }
-    }
-    void push_forward(tn value){
-        if (first == nullptr){
-            auto *node = new Node<tn>;
-            node->prev = nullptr;
-            node->next = nullptr;
-            node->value = value;
-            first = node;
-            last = node;
-        } else {
-            auto *node = new Node<tn>;
-            node->prev = last;
-            node->next = first;
-            node->value = value;
             first->prev = node;
-            first = node;
+        }
+        node->value = value;
+        node->next = first;
+        node->prev = last;
+        return node;
+    }
+public:
+    void PushBack(tn value){
+        last = PushElement(value);
+    }
+    void PushForward(tn value){
+        first = PushElement(value);
+    }
+    void PushByIndex(int number, tn value){
+        if (number == 0){
+            PushForward(value);
+        } else if (number == size()-1){
+            PushBack(value);
+        } else if (number < size()-1){
+            int counter = 0;
+            Node<tn>* temp = first;
+            while (counter != number){
+                counter++;
+                temp = temp->next;
+            }
+            auto* node = new Node<tn>;
+            node->value = value;
+            temp->next->prev = node;
+            node->next = temp->next;
+            temp->next = node;
+            node->prev = temp;
+        } else {
+            std::cout << "Index out of range" << std::endl;
         }
     }
     unsigned int size(){
@@ -87,18 +95,73 @@ public:
         }
         std::cout << iter->value << std::endl;
     }
-    void remove(int n){
-        if(n == 0){
-            first = first->next;
-            delete first->prev;
-        } else if(n == size()-1){
-            last = last->prev;
-            delete last->next;
+    tn remove(int n){
+        tn value;
+        if (n == 0){
+            last->next = first->next;
+            value = first->value;
+            delete first;
+            first = last->next;
+            first->prev = last;
+        } else if (n == size()-1){
+            first->prev = last->prev;
+            value = last->value;
+            delete last;
+            last = first->prev;
+            last->next = first;
         } else {
             Node<tn>* TempNode = FindNode(n);
             TempNode->prev->next = TempNode->next;
             TempNode->next->prev = TempNode->prev;
+            value = TempNode->value;
             delete TempNode;
+        }
+        return value;
+    }
+    int find(tn value){ //функция find озращает номер элемента который бы передан в неё. В противном случае возращается -1
+        int counter = 0;
+        Node<tn>* temp = first;
+        while (counter != size()-1){
+            if (temp->value == value){
+                return counter;
+            }
+            counter++;
+            temp = temp->next;
+        }
+        return -1;
+    }
+    int FindByAddress(Node<tn>* node){
+        int counter = 0;
+        bool flag = false;
+        Node<tn>* iter = first;
+        while (iter != last){
+            if (iter == node){
+                flag = true;
+                break;
+            }
+            iter = iter->next;
+            counter++;
+        }
+        if (!flag){
+            return -1;
+        } else {
+            return counter;
+        }
+    }
+    void PushByAddress(Node<tn>* node, tn value){
+        int position = FindByAddress(node);
+        if (position == -1) {
+            std::cout << "Element not found" << std::endl;
+        } else {
+            PushByIndex(position, value);
+        }
+    }
+    tn PopByAddress(Node<tn>* node){
+        int position = FindByAddress(node);
+        if (position == -1) {
+            std::cout << "Element not found" << std::endl;
+        } else {
+            return remove(position);
         }
     }
     tn operator [] (int n) {
@@ -106,19 +169,23 @@ public:
     }
 };
 int main() {
-    //Не пишите только так, чтобы элементы былb вне списка, иначе программа сломается :)
-
+    //Не пишите только так, чтобы элементы был вне списка, иначе программа сломается :)
+    //для пользовательского типа нужно будет перегрузить операторы << и ==
     //Блок тестов для проверки раоты программы со строками
     List <std::string> Mylist;
-    Mylist.push_back("World");
-    Mylist.push_forward("Hello");
-    Mylist.push_back("I'm");
+    Mylist.PushBack("World");
+    Mylist.PushForward("Hello");
+    Mylist.PushBack("Matvey");
+    Mylist.PushByIndex(1, "I'm");
+    Mylist.PushByIndex(5, "and");
     std::string str = Mylist[0];
+    std::cout << Mylist.find("World") << std::endl;
+    std::cout << Mylist.find("Worlddfw") << std::endl;
     std::cout << Mylist.size() << std::endl;
     Mylist.print();
-    Mylist.remove(0);
+    std::cout << Mylist.remove(1) << std::endl;
     Mylist.print();
-    Mylist.remove(1);
+    std::cout << Mylist.remove(1) << std::endl;
     Mylist.print();
     Mylist.clear();
     std::cout << Mylist.size() << std::endl;
